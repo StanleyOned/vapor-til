215563ce45572819471c8cb51278657d59d6230b
Stanle De La Cruz <stanle@prolificinteractive.com>
2018-10-01 11:52:28 -0400

    Handle Search and first query

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,4 +1,5 @@
 import Vapor
+import Fluent
 
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
@@ -30,6 +31,7 @@ public func routes(_ router: Router) throws {
         
         return try req.parameters.next(Acronym.self)
     }
+    // PUT
     // Register a route for a PUT request to /api/acronyms/<ID> that returns
     // Future<Acronym>
     router.put("api", "acronyms", Acronym.parameter) { req -> Future<Acronym> in
@@ -45,6 +47,7 @@ public func routes(_ router: Router) throws {
                             return acronym.save(on: req)
         }
     }
+    // DELETE
     // Register a route for a DELETE request to /api/acronyms/<ID> that returns
     // Future<HTTPStatus>
     router.delete("api", "acronyms", Acronym.parameter) { req -> Future<HTTPStatus> in
@@ -54,5 +57,39 @@ public func routes(_ router: Router) throws {
             .delete(on: req)
             // Transform the result into a 204 No Content response. This tells the client the request has successfully completed but there’s no content to return
             .transform(to: HTTPStatus.noContent)
+    }
+    
+    //SEARCH
+    // Register a new route handler for /api/acronyms/search that returns
+    // Future<[Acronym]>
+    router.get("api", "acronyms", "search") { req -> Future<[Acronym]> in
+        // Retrieve the search term from the URL query string
+        guard let searchTerm = req.query[String.self, at: "term"] else {
+            throw Abort(.badRequest)
+        }
+        // Use group to find all acronyms whose short and long property matches the searchTerm
+        return Acronym.query(on: req).group(.or, closure: { or in
+            or.filter(\.short == searchTerm)
+            or.filter(\.long == searchTerm)
+        }).all()
+    }
+    handleFirstResult(router: router)
+}
+
+func handleFirstResult(router: Router) {
+    // Register a new HTTP GET route for /api/acronyms/first that returns
+    // Future<Acronym>.
+    router.get("api", "acronyms", "first") { req -> Future<Acronym> in
+        // Perform a query to get the first acronym. Use the map(to:) function to unwrap the
+        // result of the query
+        return Acronym.query(on: req).first().map(to: Acronym.self, { acronym  in
+            
+            // Ensure an acronym exists. first() returns an optional as there may be no acronyms
+            // in the database. Throw a 404 Not Found error if no acronym is returned.
+            guard let acronym = acronym else {
+                throw Abort(.notFound)
+            }
+            return acronym
+        })
     }
 }
